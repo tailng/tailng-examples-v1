b517b1cc3fd423b5eabaec05919ad571a3917cbc
Prince Francis <princekfrancis@gmail.com>
2026-02-05 13:50:22 +0530

    text input demo.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {TngTextInput} from '@tailng-ui/ui/form'    
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngTextInput  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

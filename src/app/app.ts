0beab8241311c6b0496df5ef61c794ab09d5d8cb
Abin Mathew <abin02066@gmail.com>
2026-02-05 15:28:23 +0530

    Basic text Input with rootKlass

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {TngTextInput} from '@tailng-ui/ui/form'    
 
 @Component({
   selector: 'app-root',
-  imports: [TngTextInput  ],
+  imports: [TngTextInput,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  login = new FormGroup({
+    userName: new FormControl('', { nonNullable: true })
+  })
 }

d50d1f9cf776f9d2778f98f1c42e9cdcc530c6a9
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-05 17:35:46 +0530

    Created a checkbox component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TngCheckbox } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngCheckbox,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  form = new FormGroup({
+    terms: new FormControl(false, { nonNullable: true }),
+    newsletter: new FormControl(false, { nonNullable: true }),
+  });
 }

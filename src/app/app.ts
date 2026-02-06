d5304b36d5587fdd51e26344b2e546f34bd0fbe6
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-06 15:37:17 +0530

    Completed Textarea components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TngTextarea } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngTextarea,ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
+  form = new FormGroup({
+    message: new FormControl('', { nonNullable: true }),
+  });
 }

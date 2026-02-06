289e43bccc9c822b0d15da4f04d58bde518acf8a
Abin Mathew <abin02066@gmail.com>
2026-02-06 17:54:04 +0530

    Number Input with Reactive Form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TngNumberInput } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngNumberInput, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  item = new FormGroup({
+    quantity: new FormControl<number | null>(10, {
+      nonNullable: false,
+      validators: [Validators.required, Validators.min(0), Validators.max(100)],
+    }),
+    price: new FormControl<number | null>(100, { nonNullable: false }),
+  });
 }

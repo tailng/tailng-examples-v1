04303a5d5415b9c277be83157e7eb8669c212c69
Abin Mathew <abin02066@gmail.com>
2026-02-06 11:11:37 +0530

    Basic Text Input with Styling Completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import {TngTextInput} from '@tailng-ui/ui/form'    
-
+import { TngTextInput} from '@tailng-ui/ui/form';
+import { TngIcon } from '@tailng-ui/icons/icon'
 @Component({
   selector: 'app-root',
-  imports: [TngTextInput,ReactiveFormsModule],
+  imports: [TngTextInput,ReactiveFormsModule,TngIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  login = new FormGroup({
-    userName: new FormControl('', { nonNullable: true })
+  address = new FormGroup({
+    place: new FormControl('', { nonNullable: true }),
+    country: new FormControl('', { nonNullable: true })
+
   })
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideIcons } from '@ng-icons/core';
+import {bootstrapSearch} from '@ng-icons/bootstrap-icons'
+ 
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideIcons({
+      bootstrapSearch
+    })
+  ]
+}).catch((err) => console.error(err));
+

7b50417983f3518be24ad1deda140e6aa6c73c1b
Abin Mathew <abin02066@gmail.com>
2026-02-06 16:58:46 +0530

    Basic Number Input with styling completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {TngNumberInput} from '@tailng-ui/ui/form'
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ReactiveFormsModule,TngNumberInput],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  item = new FormGroup({
+    quantity: new FormControl<number | null>(10, { nonNullable: false }),
+    amount: new FormControl<number | null>(null, { nonNullable: false }),
+  });
 }

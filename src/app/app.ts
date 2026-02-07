f3c90f7d39ff48fd3f8a61122ba922bf941fec76
Abin Mathew <abin02066@gmail.com>
2026-02-07 13:04:39 +0530

    Finish autocomplete component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TngAutocomplete } from '@tailng-ui/ui/form';
+import { Country, COUNTRY_LIST, toFlagEmoji } from './shared/country-list';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngAutocomplete, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  address = new FormGroup({
+    country: new FormControl<Country | null>(null),
+  });
+  options = signal<Country[]>([]);
+  displayCountry = (c: Country) => `${toFlagEmoji(c.code)} ${c.name}`;
+  
+  displayCountryText = (c: Country) => `(${c.code}) ${c.name}`;
+
+  onSearch(term: string) {
+    const v = term.toLowerCase().trim();
+    this.options.set(v ? COUNTRY_LIST.filter((c) => c.name.toLowerCase().includes(v) || c.iso.toLowerCase().includes(v)) : []);
+  }
+
+  
 }

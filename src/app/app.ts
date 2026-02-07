8577470aa6d9e06357e837c04311c3234c56d99b
Abin Mathew <abin02066@gmail.com>
2026-02-07 21:59:49 +0530

    Implement button component and layout, styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import {TngButton} from '@tailng-ui/ui/primitives'
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

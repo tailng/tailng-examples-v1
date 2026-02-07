bb9fcc98b69f6f3d31dbfd7542a60dc82f0f8ed8
Abin Mathew <abin02066@gmail.com>
2026-02-07 14:09:04 +0530

    Implement Chips component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {TngChips} from '@tailng-ui/ui/form'
+import { Country, COUNTRY_LIST, toFlagEmoji } from './shared/country-list';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngChips,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  address = new FormGroup({
+    countries: new FormControl<Country[]>([], { nonNullable: true }),
+  });
+
+  options = signal<Country[]>(COUNTRY_LIST);
+  displayCountry = (c: Country) => `${toFlagEmoji(c.code)} ${c.name}`;
+
+  onSearch(term: string) {
+    const v = term.toLowerCase().trim();
+    this.options.set(v ? COUNTRY_LIST.filter((c) => c.name.toLowerCase().includes(v) || c.iso.toLowerCase().includes(v)) : COUNTRY_LIST);
+  }
 }

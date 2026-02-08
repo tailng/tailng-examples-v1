648068e6ef4522fde5a9a25f21dfca7fe862deb4
Abin Mathew <abin02066@gmail.com>
2026-02-08 23:12:55 +0530

    feat: Complete tabs component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { TngTab, TngTabPanel, TngTabs } from '@tailng-ui/ui/layout'
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngTabs,TngTabPanel,TngTab],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly tabValue = signal<string>('overview');
+
 }

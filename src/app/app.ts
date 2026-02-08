ea0c330cbac4fa9821334a9ed8ae48218597a725
Abin Mathew <abin02066@gmail.com>
2026-02-08 23:53:18 +0530

    feat: complete copy button component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
+import { TngCopyButton } from '@tailng-ui/ui/utilities';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ TngCopyButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  snippet = computed(() => 'Text to copy');
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { bootstrapCheck2Circle } from '@ng-icons/bootstrap-icons'
+import { provideIcons} from '@ng-icons/core'
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App,{
+  providers:[
+    provideIcons({
+    bootstrapCheck2Circle
+    }),
+  ],
+}).catch((err) => console.error(err));

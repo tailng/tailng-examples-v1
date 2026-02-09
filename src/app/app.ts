d761db54bdc5a20e5ea583db2427a127bbbd90fb
Abin Mathew <abin02066@gmail.com>
2026-02-10 00:15:23 +0530

    feat : Dialog component completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngDialog, TngDialogCloseReason } from '@tailng-ui/ui/overlay';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ TngDialog],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly open = signal(false);
+  readonly openTwo = signal(false);
+
+ onClosed(_reason: TngDialogCloseReason) {
+    this.open.set(false);
+  }
 }

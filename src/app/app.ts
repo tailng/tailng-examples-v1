61980bd1921546b79f8dca9a357daa5dac7aa08f
Abin Mathew <abin02066@gmail.com>
2026-02-09 23:33:38 +0530

    feat : Stepper component completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngStepper, TngStep, TngStepPanel } from '@tailng-ui/ui/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ TngStep ,TngStepper,TngStepPanel],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly index = signal(0);
+  readonly maxStep = 1;
+
+  prev() {
+    this.index.update((i) => Math.max(0, i - 1));
+  }
+  next() {
+    this.index.update((i) => Math.min(this.maxStep, i + 1));
+  }
+
 }

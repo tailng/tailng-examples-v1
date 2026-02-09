e018cdce79297bc0edb5163132d217f0a0f866c0
Abin Mathew <abin02066@gmail.com>
2026-02-09 23:45:53 +0530

    feat : Breadcrumbs component completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngBreadcrumbItem, TngBreadcrumbs } from '@tailng-ui/ui/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ TngBreadcrumbs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  itemsOneList = signal<TngBreadcrumbItem[]>([
+  { label: 'Projects', route: '/projects' },
+  { label: 'Tailng UI' },
+]);
+readonly itemsTwoList = signal<TngBreadcrumbItem[]>([
+    { label: 'Home', route: '/' },
+    { label: 'Docs', route: '/docs' },
+    { label: 'Breadcrumbs' },
+  ]);
 }

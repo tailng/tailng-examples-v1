2a5a7c9ebd17db2af4b2989ca9c0d445fa5f7f65
Abin Mathew <abin02066@gmail.com>
2026-02-09 19:19:54 +0530

    feat : Sidenav component completed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngSidenav, TngSidenavHeaderSlot, TngSidenavFooterSlot } from '@tailng-ui/ui/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngSidenav,TngSidenavHeaderSlot,TngSidenavFooterSlot],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  collapsed = signal(false);
 }

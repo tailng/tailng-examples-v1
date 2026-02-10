b9e6de3e7eb996ca7f577e491f36cb46efcb4da5
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 11:37:58 +0530

    feat:Popover component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TngPopover } from '@tailng-ui/ui/overlay';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngPopover],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly popoverOpen = signal(false);
+  
 }

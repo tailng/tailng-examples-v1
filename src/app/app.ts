921a345b57e8f18472c046288833f32f429eb432
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 16:39:06 +0530

    feat:Slider component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { TngSlider } from '@tailng-ui/ui/form';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngSlider, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+form = new FormGroup({
+  volume: new FormControl(50, { nonNullable: true }),
+});
+
 }

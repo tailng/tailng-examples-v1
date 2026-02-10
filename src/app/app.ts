0d203041908568a23638799648b08b6faa0ffb6f
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 17:04:05 +0530

    feat:Button toggle component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { TngButtonToggle } from '@tailng-ui/ui/form';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngButtonToggle, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  viewOptions = [
+    { value: 'list', label: 'List' },
+    { value: 'grid', label: 'Grid' },
+    { value: 'map', label: 'Map' },
+  ];
+  viewOptionsWithDisabled = [
+    { value: 'list', label: 'List' },
+    { value: 'grid', label: 'Grid', disabled: true },
+    { value: 'map', label: 'Map' },
+  ];
+  form = new FormGroup({
+    view: new FormControl<string | null>('list'),
+  });
 }

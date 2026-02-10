406f22bc400b8b86383f8c19954ca1da1c6ac4f2
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 16:04:24 +0530

    feat:Radio button component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TngRadioButton } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngRadioButton, ReactiveFormsModule],       
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  
+form = new FormGroup({
+  choice: new FormControl<string | null>(null),
+   acount: new FormControl<string | null>(null),
+   subscription: new FormControl<string | null>(null),
+});
+
 }

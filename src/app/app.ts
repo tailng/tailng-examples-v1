07a85520bbf40101566c9a0801dddf4d5e9bc01d
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 16:22:41 +0530

    feat:Select component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TngSelect } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngSelect,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+form = new FormGroup({
+  fruit: new FormControl<string | null>(null),
+});
+displayFruit = (v: string) => v;
 }

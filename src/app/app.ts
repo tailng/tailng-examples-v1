92c83bfd0703ad7bb7e45ce385e0809f3d7e0514
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-10 12:18:14 +0530

    feat:Form field components completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TngFormField, TngTextInput } from '@tailng-ui/ui/form';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngFormField, TngTextInput, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  form = new FormGroup({
+    email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+  });
 }

2ca8d6e2df855344587d29809439d2718800bdea
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-11 12:54:12 +0530

    feat:Progress spinner component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TngProgressSpinner } from '@tailng-ui/ui/primitives';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngProgressSpinner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  mode: 'indeterminate' | 'determinate' = 'indeterminate';
+  value = 40;
+  max = 100;
+
+  sizeKlass = 'w-10 h-10';
+  trackKlass = 'text-border';
+  indicatorKlass = 'text-primary';
 }

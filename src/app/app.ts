ff8486ed3fa44b1d3cc18cefb4f4beb985e23172
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-11 12:18:17 +0530

    feat:Ripples component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { TngRipple } from '@tailng-ui/ui/primitives';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngRipple],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  disabled = false;
+  centered = false;
+
+  color = 'currentColor';
+  opacity = 0.18;
+  duration = 550;
 }

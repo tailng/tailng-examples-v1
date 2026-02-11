1b027d81b26e5f9d3a94c08072684c610084a0c0
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-11 10:38:01 +0530

    feat:File upload component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { TngFileUpload } from '@tailng-ui/ui/form';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngFileUpload, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  form = new FormGroup({
+    files: new FormControl<FileList | null>(null),
+  });
 }

db2149b394a0943b8503e85ce4008019d134351b
Adarsh_E <adarshedathodathil@gmail.com>
2026-02-11 12:40:32 +0530

    feat:Progress bar component completed.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { TngProgressBar } from '@tailng-ui/ui/primitives';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TngProgressBar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
+  mode: 'determinate' | 'indeterminate' = 'determinate';
+  value = 40;
+  max = 100;
+
+  disableAnimation = false;
+
+  // klass hooks
+  rootKlass = 'w-full';
+  trackKlass = 'bg-border';
+  barKlass = 'bg-primary';
+  heightKlass = 'h-1';
 }
